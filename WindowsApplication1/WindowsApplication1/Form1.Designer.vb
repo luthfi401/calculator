1fc57fd97dbacbbc62765c2b07caad6bda7873bd
luthfi401 <luthfi.widyaeka@gmail.com>
2015-10-15 12:08:55 +0700

    final projek

--- WindowsApplication1/WindowsApplication1/Form1.Designer.vb
+++ WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -22,9 +22,411 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btn_c = New System.Windows.Forms.Button()
+        Me.btn_sqrt = New System.Windows.Forms.Button()
+        Me.btn_1 = New System.Windows.Forms.Button()
+        Me.btn_0 = New System.Windows.Forms.Button()
+        Me.btn_9 = New System.Windows.Forms.Button()
+        Me.btn_8 = New System.Windows.Forms.Button()
+        Me.btn_5 = New System.Windows.Forms.Button()
+        Me.btn_4 = New System.Windows.Forms.Button()
+        Me.btn_2 = New System.Windows.Forms.Button()
+        Me.btn_ttk = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.btn_3 = New System.Windows.Forms.Button()
+        Me.btn_6 = New System.Windows.Forms.Button()
+        Me.btn_7 = New System.Windows.Forms.Button()
+        Me.btn_smdgn = New System.Windows.Forms.Button()
+        Me.btn_krng = New System.Windows.Forms.Button()
+        Me.btn_kali = New System.Windows.Forms.Button()
+        Me.btn_bagi = New System.Windows.Forms.Button()
+        Me.btn_plus = New System.Windows.Forms.Button()
+        Me.btn_pluskrg = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn_c
+        '
+        Me.btn_c.Location = New System.Drawing.Point(12, 72)
+        Me.btn_c.Name = "btn_c"
+        Me.btn_c.Size = New System.Drawing.Size(45, 45)
+        Me.btn_c.TabIndex = 0
+        Me.btn_c.Text = "c"
+        Me.btn_c.UseVisualStyleBackColor = True
+        '
+        'btn_sqrt
+        '
+        Me.btn_sqrt.Location = New System.Drawing.Point(63, 72)
+        Me.btn_sqrt.Name = "btn_sqrt"
+        Me.btn_sqrt.Size = New System.Drawing.Size(45, 45)
+        Me.btn_sqrt.TabIndex = 1
+        Me.btn_sqrt.Text = "sqrt"
+        Me.btn_sqrt.UseVisualStyleBackColor = True
+        '
+        'btn_1
+        '
+        Me.btn_1.Location = New System.Drawing.Point(12, 225)
+        Me.btn_1.Name = "btn_1"
+        Me.btn_1.Size = New System.Drawing.Size(45, 45)
+        Me.btn_1.TabIndex = 2
+        Me.btn_1.Text = "1"
+        Me.btn_1.UseVisualStyleBackColor = True
+        '
+        'btn_0
+        '
+        Me.btn_0.Location = New System.Drawing.Point(12, 276)
+        Me.btn_0.Name = "btn_0"
+        Me.btn_0.Size = New System.Drawing.Size(45, 45)
+        Me.btn_0.TabIndex = 3
+        Me.btn_0.Text = "0"
+        Me.btn_0.UseVisualStyleBackColor = True
+        '
+        'btn_9
+        '
+        Me.btn_9.Location = New System.Drawing.Point(12, 123)
+        Me.btn_9.Name = "btn_9"
+        Me.btn_9.Size = New System.Drawing.Size(45, 45)
+        Me.btn_9.TabIndex = 4
+        Me.btn_9.Text = "9"
+        Me.btn_9.UseVisualStyleBackColor = True
+        '
+        'btn_8
+        '
+        Me.btn_8.Location = New System.Drawing.Point(63, 123)
+        Me.btn_8.Name = "btn_8"
+        Me.btn_8.Size = New System.Drawing.Size(45, 45)
+        Me.btn_8.TabIndex = 5
+        Me.btn_8.Text = "8"
+        Me.btn_8.UseVisualStyleBackColor = True
+        '
+        'btn_5
+        '
+        Me.btn_5.Location = New System.Drawing.Point(63, 174)
+        Me.btn_5.Name = "btn_5"
+        Me.btn_5.Size = New System.Drawing.Size(45, 45)
+        Me.btn_5.TabIndex = 6
+        Me.btn_5.Text = "5"
+        Me.btn_5.UseVisualStyleBackColor = True
+        '
+        'btn_4
+        '
+        Me.btn_4.Location = New System.Drawing.Point(12, 174)
+        Me.btn_4.Name = "btn_4"
+        Me.btn_4.Size = New System.Drawing.Size(45, 45)
+        Me.btn_4.TabIndex = 7
+        Me.btn_4.Text = "4"
+        Me.btn_4.UseVisualStyleBackColor = True
+        '
+        'btn_2
+        '
+        Me.btn_2.Location = New System.Drawing.Point(63, 225)
+        Me.btn_2.Name = "btn_2"
+        Me.btn_2.Size = New System.Drawing.Size(45, 45)
+        Me.btn_2.TabIndex = 8
+        Me.btn_2.Text = "2"
+        Me.btn_2.UseVisualStyleBackColor = True
+        '
+        'btn_ttk
+        '
+        Me.btn_ttk.Location = New System.Drawing.Point(63, 276)
+        Me.btn_ttk.Name = "btn_ttk"
+        Me.btn_ttk.Size = New System.Drawing.Size(45, 45)
+        Me.btn_ttk.TabIndex = 9
+        Me.btn_ttk.Text = "."
+        Me.btn_ttk.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 21)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(198, 45)
+        Me.TextBox1.TabIndex = 10
+        '
+        'btn_3
+        '
+        Me.btn_3.Location = New System.Drawing.Point(114, 225)
+        Me.btn_3.Name = "btn_3"
+        Me.btn_3.Size = New System.Drawing.Size(45, 45)
+        Me.btn_3.TabIndex = 11
+        Me.btn_3.Text = "3"
+        Me.btn_3.UseVisualStyleBackColor = True
+        '
+        'btn_6
+        '
+        Me.btn_6.Location = New System.Drawing.Point(114, 174)
+        Me.btn_6.Name = "btn_6"
+        Me.btn_6.Size = New System.Drawing.Size(45, 45)
+        Me.btn_6.TabIndex = 12
+        Me.btn_6.Text = "6"
+        Me.btn_6.UseVisualStyleBackColor = True
+        '
+        'btn_7
+        '
+        Me.btn_7.Location = New System.Drawing.Point(114, 123)
+        Me.btn_7.Name = "btn_7"
+        Me.btn_7.Size = New System.Drawing.Size(45, 45)
+        Me.btn_7.TabIndex = 13
+        Me.btn_7.Text = "7"
+        Me.btn_7.UseVisualStyleBackColor = True
+        '
+        'btn_smdgn
+        '
+        Me.btn_smdgn.Location = New System.Drawing.Point(114, 276)
+        Me.btn_smdgn.Name = "btn_smdgn"
+        Me.btn_smdgn.Size = New System.Drawing.Size(96, 45)
+        Me.btn_smdgn.TabIndex = 15
+        Me.btn_smdgn.Text = "="
+        Me.btn_smdgn.UseVisualStyleBackColor = True
+        '
+        'btn_krng
+        '
+        Me.btn_krng.Location = New System.Drawing.Point(165, 174)
+        Me.btn_krng.Name = "btn_krng"
+        Me.btn_krng.Size = New System.Drawing.Size(45, 45)
+        Me.btn_krng.TabIndex = 16
+        Me.btn_krng.Text = "-"
+        Me.btn_krng.UseVisualStyleBackColor = True
+        '
+        'btn_kali
+        '
+        Me.btn_kali.Location = New System.Drawing.Point(165, 123)
+        Me.btn_kali.Name = "btn_kali"
+        Me.btn_kali.Size = New System.Drawing.Size(45, 45)
+        Me.btn_kali.TabIndex = 17
+        Me.btn_kali.Text = "*"
+        Me.btn_kali.UseVisualStyleBackColor = True
+        '
+        'btn_bagi
+        '
+        Me.btn_bagi.Location = New System.Drawing.Point(165, 72)
+        Me.btn_bagi.Name = "btn_bagi"
+        Me.btn_bagi.Size = New System.Drawing.Size(45, 45)
+        Me.btn_bagi.TabIndex = 18
+        Me.btn_bagi.Text = "/"
+        Me.btn_bagi.UseVisualStyleBackColor = True
+        '
+        'btn_plus
+        '
+        Me.btn_plus.Location = New System.Drawing.Point(165, 225)
+        Me.btn_plus.Name = "btn_plus"
+        Me.btn_plus.Size = New System.Drawing.Size(45, 45)
+        Me.btn_plus.TabIndex = 19
+        Me.btn_plus.Text = "+"
+        Me.btn_plus.UseVisualStyleBackColor = True
+        '
+        'btn_pluskrg
+        '
+        Me.btn_pluskrg.Location = New System.Drawing.Point(114, 72)
+        Me.btn_pluskrg.Name = "btn_pluskrg"
+        Me.btn_pluskrg.Size = New System.Drawing.Size(45, 45)
+        Me.btn_pluskrg.TabIndex = 20
+        Me.btn_pluskrg.Text = "+-"
+        Me.btn_pluskrg.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 324)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 21
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(57, 324)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 22
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(102, 324)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 23
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(224, 343)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btn_pluskrg)
+        Me.Controls.Add(Me.btn_plus)
+        Me.Controls.Add(Me.btn_bagi)
+        Me.Controls.Add(Me.btn_kali)
+        Me.Controls.Add(Me.btn_krng)
+        Me.Controls.Add(Me.btn_smdgn)
+        Me.Controls.Add(Me.btn_7)
+        Me.Controls.Add(Me.btn_6)
+        Me.Controls.Add(Me.btn_3)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btn_ttk)
+        Me.Controls.Add(Me.btn_2)
+        Me.Controls.Add(Me.btn_4)
+        Me.Controls.Add(Me.btn_5)
+        Me.Controls.Add(Me.btn_8)
+        Me.Controls.Add(Me.btn_9)
+        Me.Controls.Add(Me.btn_0)
+        Me.Controls.Add(Me.btn_1)
+        Me.Controls.Add(Me.btn_sqrt)
+        Me.Controls.Add(Me.btn_c)
+        Me.Name = "Form1"
+        Me.Text = "Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btn_c As System.Windows.Forms.Button
+    Friend WithEvents btn_sqrt As System.Windows.Forms.Button
+    Friend WithEvents btn_1 As System.Windows.Forms.Button
+    Friend WithEvents btn_0 As System.Windows.Forms.Button
+    Friend WithEvents btn_9 As System.Windows.Forms.Button
+    Friend WithEvents btn_8 As System.Windows.Forms.Button
+    Friend WithEvents btn_5 As System.Windows.Forms.Button
+    Friend WithEvents btn_4 As System.Windows.Forms.Button
+    Friend WithEvents btn_2 As System.Windows.Forms.Button
+    Friend WithEvents btn_ttk As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents btn_3 As System.Windows.Forms.Button
+    Friend WithEvents btn_6 As System.Windows.Forms.Button
+    Friend WithEvents btn_7 As System.Windows.Forms.Button
+    Friend WithEvents btn_smdgn As System.Windows.Forms.Button
+    Friend WithEvents btn_krng As System.Windows.Forms.Button
+    Friend WithEvents btn_kali As System.Windows.Forms.Button
+    Friend WithEvents btn_bagi As System.Windows.Forms.Button
+    Friend WithEvents btn_plus As System.Windows.Forms.Button
+    Friend WithEvents btn_pluskrg As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+
+    Private Sub btn_c_Click(sender As Object, e As EventArgs) Handles btn_c.Click
+        TextBox1.Clear()
+        Label1.Text = ""
+        Label2.Text = ""
+        Label3.Text = ""
+
+
+
     End Sub
 
+    Private Sub btn_sqrt_Click(sender As Object, e As EventArgs) Handles btn_sqrt.Click
+        Dim sqrt As Double
+        sqrt = Convert.ToDouble(TextBox1.Text)
+        TextBox1.Text = Convert.ToString(Math.Sqrt(sqrt))
+
+
+    End Sub
+
+    Private Sub btn_pluskrg_Click(sender As Object, e As EventArgs) Handles btn_pluskrg.Click
+        Dim numb As Double
+        numb = Convert.ToDouble(TextBox1.Text)
+        Dim nu As Double
+        nu = numb - numb - numb
+        TextBox1.Text = Convert.ToString(nu)
+
+    End Sub
+
+    Private Sub btn_9_Click(sender As Object, e As EventArgs) Handles btn_9.Click
+        TextBox1.AppendText(9)
+    End Sub
+
+    Private Sub btn_8_Click(sender As Object, e As EventArgs) Handles btn_8.Click
+        TextBox1.AppendText(8)
+    End Sub
+
+    Private Sub btn_7_Click(sender As Object, e As EventArgs) Handles btn_7.Click
+        TextBox1.AppendText(7)
+    End Sub
+
+    Private Sub btn_4_Click(sender As Object, e As EventArgs) Handles btn_4.Click
+        TextBox1.AppendText(4)
+    End Sub
+
+    Private Sub btn_5_Click(sender As Object, e As EventArgs) Handles btn_5.Click
+        TextBox1.AppendText(5)
+    End Sub
+
+    Private Sub btn_6_Click(sender As Object, e As EventArgs) Handles btn_6.Click
+        TextBox1.AppendText(6)
+    End Sub
+
+    Private Sub btn_1_Click(sender As Object, e As EventArgs) Handles btn_1.Click
+        TextBox1.AppendText(1)
+    End Sub
+
+    Private Sub btn_2_Click(sender As Object, e As EventArgs) Handles btn_2.Click
+        TextBox1.AppendText(2)
+    End Sub
+
+    Private Sub btn_3_Click(sender As Object, e As EventArgs) Handles btn_3.Click
+        TextBox1.AppendText(3)
+    End Sub
+
+    Private Sub btn_0_Click(sender As Object, e As EventArgs) Handles btn_0.Click
+        TextBox1.AppendText(0)
+    End Sub
+
+    Private Sub btn_ttk_Click(sender As Object, e As EventArgs) Handles btn_ttk.Click
+        TextBox1.AppendText(".")
+    End Sub
+
+    Private Sub btn_plus_Click(sender As Object, e As EventArgs) Handles btn_plus.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "+"
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub btn_krng_Click(sender As Object, e As EventArgs) Handles btn_krng.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "-"
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub btn_kali_Click(sender As Object, e As EventArgs) Handles btn_kali.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "*"
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub btn_bagi_Click(sender As Object, e As EventArgs) Handles btn_bagi.Click
+        Label1.Text = TextBox1.Text
+        Label2.Text = "/"
+        TextBox1.Clear()
+    End Sub
+
+    Private Sub btn_smdgn_Click(sender As Object, e As EventArgs) Handles btn_smdgn.Click
+        Label3.Text = TextBox1.Text
+        Dim sign As Char
+        sign = Label2.Text
+        Dim n1 As Double
+        n1 = Convert.ToDouble(Label1.Text)
+        Dim n2 As Double
+        n2 = Convert.ToDouble(Label3.Text)
+        Dim n3 As Double
+
+        Select Case (sign)
+            Case "+"
+                n3 = n1 + n2
+            Case "-"
+                n3 = n1 - n2
+            Case "*"
+                n3 = n1 * n2
+            Case "/"
+                n3 = n1 / n2
+
+        End Select
+        TextBox1.Text = Convert.ToString(n3)
+    End Sub
 End Class
